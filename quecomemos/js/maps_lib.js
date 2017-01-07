b6f0bba24d6904eb1438c1904765ae381a96125f
Nicolas Perez <nicolasadriaan@gmail.com>
2017-01-06 23:26:38 -0300

    Add files via upload

---
+++ quecomemos/js/maps_lib.js
@@ -0,0 +1,428 @@
+(function (window, undefined) {
+    var MapsLib = function (options) {
+        var self = this;
+
+        options = options || {};
+
+        this.recordName = options.recordName || "registro"; //for showing a count of results
+        this.recordNamePlural = options.recordNamePlural || "registros";
+        this.searchRadius = options.searchRadius || 805; //in meters ~ 1/2 mile
+
+        // the encrypted Table ID of your Fusion Table (found under File => About)
+        this.fusionTableId = options.fusionTableId || "1LLpwbXL4Kj2jvKuI-z0mu7MWdW54Y5FhUGypNFyJ",
+
+        // Found at https://console.developers.google.com/
+        // Important! this key is for demonstration purposes. please register your own.
+        this.googleApiKey = options.googleApiKey || "AIzaSyCirQVWeLi8N5pqqAGJpCMFWjP_n4UpqNU",
+        
+        // name of the location column in your Fusion Table.
+        // NOTE: if your location column name has spaces in it, surround it with single quotes
+        // example: locationColumn:     "'my location'",
+        this.locationColumn = options.locationColumn || "col27";
+        
+        // appends to all address searches if not present
+        this.locationScope = options.locationScope || "";
+
+        // zoom level when map is loaded (bigger is more zoomed in)
+        this.defaultZoom = options.defaultZoom || 11; 
+
+        // center that your map defaults to
+        this.map_centroid = new google.maps.LatLng(options.map_center[0], options.map_center[1]);
+        
+        // marker image for your searched address
+        if (typeof options.addrMarkerImage !== 'undefined') {
+            if (options.addrMarkerImage != "")
+                this.addrMarkerImage = options.addrMarkerImage;
+            else
+                this.addrMarkerImage = ""
+        }
+        else
+            this.addrMarkerImage = "images/blue-pushpin.png"
+
+    	this.currentPinpoint = null;
+    	$("#result_count").html("");
+        
+        this.myOptions = {
+            zoom: this.defaultZoom,
+            center: this.map_centroid,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        this.geocoder = new google.maps.Geocoder();
+        this.map = new google.maps.Map($("#map")[0], this.myOptions);
+        
+        // maintains map centerpoint for responsive design
+        google.maps.event.addDomListener(self.map, 'idle', function () {
+            self.calculateCenter();
+        });
+        google.maps.event.addDomListener(window, 'resize', function () {
+            self.map.setCenter(self.map_centroid);
+        });
+        self.searchrecords = null;
+
+        //reset filters
+        $("#search_address").val(self.convertToPlainString($.address.parameter('address')));
+		 $("#text_search").val(self.convertToPlainString($.address.parameter('text_search'))); //se agrega esta linea para que limpie el registro de busqueda cada vez que se agregue un box de busqueda por texto
+		 $("#text_search1").val(self.convertToPlainString($.address.parameter('text_search1'))); //se agrega esta linea para que limpie el registro de busqueda cada vez que se agregue un box de busqueda por texto 
+		 $("#text_search2").val(self.convertToPlainString($.address.parameter('text_search2'))); //se agrega esta linea para que limpie el registro de busqueda cada vez que se agregue un box de busqueda por texto
+		
+		 $("#text_search3").val(self.convertToPlainString($.address.parameter('text_search3'))); //se agrega esta linea para que limpie el registro de busqueda cada vez que se agregue un box de busqueda por texto
+		
+		 $("#text_search4").val(self.convertToPlainString($.address.parameter('text_search4'))); //se agrega esta linea para que limpie el registro de busqueda cada vez que se agregue un box de busqueda por texto
+		
+        var loadRadius = self.convertToPlainString($.address.parameter('radius'));
+        if (loadRadius != "") 
+            $("#search_radius").val(loadRadius);
+        else 
+            $("#search_radius").val(self.searchRadius);
+        
+        $(":checkbox").prop("checked", "checked");
+        $("#result_box").hide();
+
+        //-----custom initializers-----
+	/*	//nuevo que se agrega
+		$("#age-slider").slider({
+    orientation: "horizontal",
+    range: true,
+    min: 2009,
+    max: 2016,
+    values: [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016],
+    step: 0,
+    slide: function (event, ui) {
+        $("#age-selected-start").html(ui.values[0]);
+      $("#age-selected-end").html(ui.values[1]);
+    },
+    stop: function(event, ui) {
+      self.doSearch();
+    }
+});//cierre de nuevo que se agrega  */
+	
+
+
+        //-----end of custom initializers-----
+
+        //run the default search when page loads
+        self.doSearch();
+        if (options.callback) options.callback(self);
+    };
+
+    //-----custom functions-----
+    //-----end of custom functions-----
+
+    MapsLib.prototype.submitSearch = function (whereClause, map) {
+        var self = this;
+        //get using all filters
+        //NOTE: styleId and templateId are recently added attributes to load custom marker styles and info windows
+        //you can find your Ids inside the link generated by the 'Publish' option in Fusion Tables
+        //for more details, see https://developers.google.com/fusiontables/docs/v1/using#WorkingStyles
+        self.searchrecords = new google.maps.FusionTablesLayer({
+            query: {
+                from: self.fusionTableId,
+                select: self.locationColumn,
+                where: whereClause
+            },
+            styleId: 2,
+            templateId: 2
+        });
+        self.fusionTable = self.searchrecords;
+        self.searchrecords.setMap(map);
+        self.getCount(whereClause);
+    };
+
+
+    MapsLib.prototype.getgeoCondition = function (address, callback) {
+        var self = this;
+        if (address !== "") {
+            if (address.toLowerCase().indexOf(self.locationScope) === -1) {
+                address = address + " " + self.locationScope;
+            }
+            self.geocoder.geocode({
+                'address': address
+            }, function (results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    self.currentPinpoint = results[0].geometry.location;
+                    var map = self.map;
+
+                    $.address.parameter('address', encodeURIComponent(address));
+                    $.address.parameter('radius', encodeURIComponent(self.searchRadius));
+                    map.setCenter(self.currentPinpoint);
+                    // set zoom level based on search radius
+                    if (self.searchRadius >= 1610000) map.setZoom(4); // 1,000 miles
+                    else if (self.searchRadius >= 805000) map.setZoom(5); // 500 miles
+                    else if (self.searchRadius >= 402500) map.setZoom(6); // 250 miles
+                    else if (self.searchRadius >= 161000) map.setZoom(7); // 100 miles
+                    else if (self.searchRadius >= 80500) map.setZoom(8); // 100 miles
+                    else if (self.searchRadius >= 40250) map.setZoom(9); // 100 miles
+                    else if (self.searchRadius >= 16100) map.setZoom(11); // 10 miles
+                    else if (self.searchRadius >= 8050) map.setZoom(12); // 5 miles
+                    else if (self.searchRadius >= 3220) map.setZoom(13); // 2 miles
+                    else if (self.searchRadius >= 1610) map.setZoom(14); // 1 mile
+                    else if (self.searchRadius >= 805) map.setZoom(15); // 1/2 mile
+                    else if (self.searchRadius >= 400) map.setZoom(16); // 1/4 mile
+                    else self.map.setZoom(17);
+
+                    if (self.addrMarkerImage != '') {
+                        self.addrMarker = new google.maps.Marker({
+                            position: self.currentPinpoint,
+                            map: self.map,
+                            icon: self.addrMarkerImage,
+                            animation: google.maps.Animation.DROP,
+                            title: address
+                        });
+                    }
+                    var geoCondition = " AND ST_INTERSECTS(" + self.locationColumn + ", CIRCLE(LATLNG" + self.currentPinpoint.toString() + "," + self.searchRadius + "))";
+                    callback(geoCondition);
+                    self.drawSearchRadiusCircle(self.currentPinpoint);
+                } else {
+                    alert("Su resultado de busqueda da: " + status);
+                    callback('');
+                }
+            });
+        } else {
+            callback('');
+        }
+    };
+
+    MapsLib.prototype.doSearch = function () {
+        var self = this;
+        self.clearSearch();
+        var address = $("#search_address").val();
+        self.searchRadius = $("#search_radius").val();
+        self.whereClause = self.locationColumn + " not equal to ''";
+        
+        //-----custom filters-----
+	/*	if ( $("#select_type").val() != "")
+        self.whereClause += " AND 'type' = '" + $("#select_type").val() + "'";*/
+	/*  self.whereClause += " AND 'col13' >= '" + $("#age-selected-start").html() + "'";
+self.whereClause += " AND 'col13' <= '" + $("#age-selected-end").html() + "'";
+*/
+	  var text_search = $("#text_search").val().replace("'", "\\'");
+if (text_search != '')
+  self.whereClause += " AND 'col11' contains ignoring case '" + text_search + "'";
+	  
+	  var text_search1 = $("#text_search1").val().replace("'", "\\'");
+if (text_search1 != '')
+  self.whereClause += " AND 'col20' contains ignoring case '" + text_search1 + "'";
+	   
+	   var text_search2 = $("#text_search2").val().replace("'", "\\'");
+if (text_search2 != '')
+  self.whereClause += " AND 'col19' contains ignoring case '" + text_search2 + "'";
+	  
+	   
+	   var text_search3 = $("#text_search3").val().replace("'", "\\'");
+if (text_search3 != '')
+  self.whereClause += " AND 'col18' contains ignoring case '" + text_search3 + "'";
+	  
+	   var text_search4 = $("#text_search4").val().replace("'", "\\'");
+if (text_search4 != '')
+  self.whereClause += " AND 'col13' contains ignoring case '" + text_search4 + "'";
+	  
+	  
+        //-----end of custom filters-----
+
+        self.getgeoCondition(address, function (geoCondition) {
+            self.whereClause += geoCondition;
+            self.submitSearch(self.whereClause, self.map);
+        });
+
+    };
+
+    MapsLib.prototype.reset = function () {
+        $.address.parameter('address','');
+        $.address.parameter('radius','');
+		
+        window.location.reload();
+    };
+
+
+    MapsLib.prototype.getInfo = function (callback) {
+        var self = this;
+        jQuery.ajax({
+            url: 'https://www.googleapis.com/fusiontables/v1/tables/' + self.fusionTableId + '?key=' + self.googleApiKey,
+            dataType: 'json'
+        }).done(function (response) {
+            if (callback) callback(response);
+        });
+    };
+
+    MapsLib.prototype.addrFromLatLng = function (latLngPoint) {
+        var self = this;
+        self.geocoder.geocode({
+            'latLng': latLngPoint
+        }, function (results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                if (results[1]) {
+                    $('#search_address').val(results[1].formatted_address);
+                    $('.hint').focus();
+                    self.doSearch();
+                }
+            } else {
+                alert("Geocoder failed due to: " + status);
+            }
+        });
+    };
+
+    MapsLib.prototype.drawSearchRadiusCircle = function (point) {
+        var self = this;
+        var circleOptions = {
+            strokeColor: "#4b58a6",
+            strokeOpacity: 0.3,
+            strokeWeight: 1,
+            fillColor: "#ffd306",
+            fillOpacity: 0.05,
+            map: self.map,
+            center: point,
+            clickable: false,
+            zIndex: -1,
+            radius: parseInt(self.searchRadius)
+        };
+        self.searchRadiusCircle = new google.maps.Circle(circleOptions);
+    };
+
+    MapsLib.prototype.query = function (query_opts, callback) {
+        var queryStr = [],
+            self = this;
+        queryStr.push("SELECT " + query_opts.select);
+        queryStr.push(" FROM " + self.fusionTableId);
+        // where, group and order clauses are optional
+        if (query_opts.where && query_opts.where != "") {
+            queryStr.push(" WHERE " + query_opts.where);
+        }
+        if (query_opts.groupBy && query_opts.groupBy != "") {
+            queryStr.push(" GROUP BY " + query_opts.groupBy);
+        }
+        if (query_opts.orderBy && query_opts.orderBy != "") {
+            queryStr.push(" ORDER BY " + query_opts.orderBy);
+        }
+        if (query_opts.offset && query_opts.offset !== "") {
+            queryStr.push(" OFFSET " + query_opts.offset);
+        }
+        if (query_opts.limit && query_opts.limit !== "") {
+            queryStr.push(" LIMIT " + query_opts.limit);
+        }
+        var theurl = {
+            base: "https://www.googleapis.com/fusiontables/v1/query?sql=",
+            queryStr: queryStr,
+            key: self.googleApiKey
+        };
+        $.ajax({
+            url: [theurl.base, encodeURIComponent(theurl.queryStr.join(" ")), "&key=", theurl.key].join(''),
+            dataType: "json"
+        }).done(function (response) {
+            //console.log(response);
+            if (callback) callback(response);
+        }).fail(function(response) {
+            self.handleError(response);
+        });
+    };
+
+    MapsLib.prototype.handleError = function (json) {
+        if (json.error !== undefined) {
+            var error = json.responseJSON.error.errors;
+            console.log("Error in Fusion Table call!");
+            for (var row in error) {
+                console.log(" Domain: " + error[row].domain);
+                console.log(" Reason: " + error[row].reason);
+                console.log(" Message: " + error[row].message);
+            }
+        }
+    };
+    MapsLib.prototype.getCount = function (whereClause) {
+        var self = this;
+        var selectColumns = "Count()";
+        self.query({
+            select: selectColumns,
+            where: whereClause
+        }, function (json) {
+            self.displaySearchCount(json);
+        });
+    };
+
+    MapsLib.prototype.displaySearchCount = function (json) {
+        var self = this;
+        
+        var numRows = 0;
+        if (json["rows"] != null) {
+            numRows = json["rows"][0];
+        }
+        var name = self.recordNamePlural;
+        if (numRows == 1) {
+            name = self.recordName;
+        }
+        $("#result_box").fadeOut(function () {
+            $("#result_count").html(self.addCommas(numRows) + " " + name + " encontrados");
+        });
+        $("#result_box").fadeIn();
+    };
+
+    MapsLib.prototype.addCommas = function (nStr) {
+        nStr += '';
+        x = nStr.split('.');
+        x1 = x[0];
+        x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ',' + '$2');
+        }
+        return x1 + x2;
+    };
+
+    // maintains map centerpoint for responsive design
+    MapsLib.prototype.calculateCenter = function () {
+        var self = this;
+        center = self.map.getCenter();
+    };
+
+    //converts a slug or query string in to readable text
+    MapsLib.prototype.convertToPlainString = function (text) {
+        if (text === undefined) return '';
+        return decodeURIComponent(text);
+    };
+
+    MapsLib.prototype.clearSearch = function () {
+        var self = this;
+        if (self.searchrecords && self.searchrecords.getMap) 
+            self.searchrecords.setMap(null);
+        if (self.addrMarker && self.addrMarker.getMap) 
+            self.addrMarker.setMap(null);
+        if (self.searchRadiusCircle && self.searchRadiusCircle.getMap) 
+            self.searchRadiusCircle.setMap(null);
+    };
+
+    MapsLib.prototype.findMe = function () {
+        var self = this;
+        var foundLocation;
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var latitude = position.coords.latitude;
+                var longitude = position.coords.longitude;
+                var accuracy = position.coords.accuracy;
+                var coords = new google.maps.LatLng(latitude, longitude);
+                self.map.panTo(coords);
+                self.addrFromLatLng(coords);
+                self.map.setZoom(14);
+                jQuery('#map').append('<div id="myposition"><i class="fontello-target"></i></div>');
+                setTimeout(function () {
+                    jQuery('#myposition').remove();
+                }, 3000);
+            }, function error(msg) {
+                alert('Please enable your GPS position future.');
+            }, {
+                //maximumAge: 600000,
+                //timeout: 5000,
+                enableHighAccuracy: true
+            });
+        } else {
+            alert("Geolocation API is not supported in your browser.");
+        }
+    };
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        module.exports = MapsLib;
+    } else if (typeof define === 'function' && define.amd) {
+        define(function () {
+            return MapsLib;
+        });
+    } else {
+        window.MapsLib = MapsLib;
+    }
+
+})(window);
