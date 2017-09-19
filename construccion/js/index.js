71267914e811c863d1a6d7f95bdcef727df642e7
Nicolas Perez <nicolasadriaan@gmail.com>
2017-09-19 00:26:56 -0300

    Add files via upload

---
+++ construccion/js/index.js
@@ -0,0 +1,8 @@
+$(function(){
+        $("#adjectives").typed({
+            strings: ["Bienvenidos.",  "Estén atentos.", "En breve abrimos.", "PD: Sitio en construcción (80%)"],
+            typeSpeed: 15,
+            loop: false,
+            backDelay: 1000
+        });
+    });
